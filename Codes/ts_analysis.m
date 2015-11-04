d5e9bf22d66673df70dfac3e9635e4617ea257e1
mossmao <mossmao@gmail.com>
2015-11-04 08:18:49 -0800

    papers and info for matlab time series analysis

---
+++ Codes/ts_analysis.m
@@ -0,0 +1,8 @@
+%% time series analysis
+
+% there are a lot build in methods in matlab for time series 
+% http://www.mathworks.com/videos/matlab-production-server-for-financial-applications-81937.html
+% 
+
+% e.g. 
+% ftseries: Financial Toolbox Times Series Functions.
