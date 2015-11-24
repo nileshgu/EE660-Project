531fedbabbb5456088036c6749d8bdf3f24dc5e0
Nilesh Gupta <nileshgu@usc.edu>
2015-11-23 17:57:30 -0800

    SVM Updated.

--- Codes/ml_SVM.m
+++ Codes/ml_SVM.m
@@ -1,20 +1,27 @@
 % The main m file
 
 %% data process
-load('../data/FeatureDataFromDepart.mat');;
+load('../data/FeatureDataFromDepartAndDate.mat');
 
 %% learning algorithm
 % TODO: adaboost here
 % split the data into three sets
 [m,n]=size(X);
-idx1 = floor(m/3);
+idx1 = floor(m/3); %random classification
 idx2 = floor(2*m/3);
-X_preTrain = X(1:idx1,:);y_preTrain = y(1:idx1,:);
-X_train = X(idx1+1:idx2,:);y_train = X(idx1+1:idx2,:);
-X_test = X(idx2+1:end,:);y_test = X(idx2+1:end,:);
+X_preTrain = X(1:idx1,:);y_preTrain = y(1:idx1);
+X_train = X(idx1+1:idx2,:);y_train = y(idx1+1:idx2);
+X_test = X(idx2+1:end,:);y_test = y(idx2+1:end);
 
 % \pmtk3-master\pmtksupportCopy\boostingDemo
 
 %% performance evaluation 
 
-%% yield the output
+
+model = svmFit(X_preTrain, y_preTrain);
+yhat  = svmPredict(model, X_preTrain);
+
+
+%% yield the output and calculate the error
+
+errorRateGen = mean(yhat ~= y_preTrain);
