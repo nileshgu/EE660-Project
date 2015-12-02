17a9da199b4474127c6e04d52dfcdc3937d1ac9d
mossmao <mossmao@gmail.com>
2015-12-02 15:57:11 -0800

    revise random forest

--- Codes/ml_Forest.m
+++ Codes/ml_Forest.m
@@ -1,13 +1,6 @@
-%% data process
-load('../data/FeatureDataFromDepartAndDate.mat');
-
-%% split the data into three sets
-[m,n]=size(X);
-idx1 = floor(m/3);
-idx2 = floor(2*m/3);
-X_preTrain = X(1:idx1,:);y_preTrain = y(1:idx1,:);
-X_train = X(idx1+1:idx2,:);y_train = y(idx1+1:idx2,:);
-X_test = X(idx2+1:end,:);y_test = y(idx2+1:end,:);
+% load data
+clear;clc;close all;
+load('../Data/FourFoldData.mat');
 
 %% run forrest fit
 addpath decisionTrees;
@@ -18,14 +11,14 @@ randomFeatures = 2;
 bagSize = 1/3;
 
 tic;
-N = 30000;
+N = size(X_preTrain,1);
 nitr = 1;
 nntree = 30;
 errs_test = zeros(nitr,nntree);
 errs_train = zeros(nitr,nntree);
-for ntree = 10:1:nntree
+for ntree = 30:1:nntree
 	ntree
-    idx_train = randperm(N);idx_train = idx_train(1:10000);
+    idx_train = randperm(N);idx_train = idx_train(1:floor(N*0.7));
     Xtrain_ = X_train(idx_train,:);
     Ytrain_ = y_train(idx_train,:);
 	for itr = 1:nitr
